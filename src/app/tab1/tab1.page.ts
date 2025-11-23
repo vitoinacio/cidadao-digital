93aa4ac774f774133ca7186c327b286791acb814
Victor Hugo Inacio de Oliveira <108891666+vitoinacio@users.noreply.github.com>
2025-11-23 20:24:24 +0000

    fix: bug do package json removido

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButtons, IonButton, IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardTitle],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardTitle],
 })
 export class Tab1Page {
   constructor() {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonButtons, IonButton, IonIcon, IonCard,IonCardContent, IonCardTitle, IonCardHeader, IonFab,IonFabButton } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonButtons, IonButton, IonIcon, IonCard,IonCardContent, IonFab,IonFabButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle,IonContent, ExploreContainerComponent, IonButtons,IonButton, IonIcon, IonCard, IonCardContent,IonCardTitle, IonCardHeader, IonFab, IonFabButton]
+  imports: [IonHeader, IonToolbar, IonTitle,IonContent, IonButtons,IonButton, IonIcon, IonCard, IonCardContent, IonFab, IonFabButton]
 })
 export class Tab2Page {
 
